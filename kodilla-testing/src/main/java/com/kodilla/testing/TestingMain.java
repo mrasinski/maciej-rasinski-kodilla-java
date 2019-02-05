0393f9317943f4447560e2711f517c75f4c1fe2c
mrasinski <ariel_6@wp.pl>
2019-02-05 20:57:43 +0100

    comments

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -8,12 +8,14 @@ public class TestingMain {
 
         Calculator calc = new Calculator();
 
+        System.out.println("Test - add");
         if(calc.add(2,3) == 5) {
             System.out.println("Test OK");
         } else {
             System.out.println("Wrong result");
         }
 
+        System.out.println("Test - substract");
         if(calc.substract(2,3) == 5) {
             System.out.println("Test OK");
         } else {
