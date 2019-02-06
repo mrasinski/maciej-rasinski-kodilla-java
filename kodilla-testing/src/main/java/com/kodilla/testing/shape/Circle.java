0f363bf5b01a61ca627d87390fbf996f37d817e8
mrasinski <ariel_6@wp.pl>
2019-02-06 20:08:02 +0100

    changes

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/Circle.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/Circle.java
@@ -1,5 +1,7 @@
 package com.kodilla.testing.shape;
 
+import java.util.Objects;
+
 public class Circle implements Shape {
 
     private double radius;
@@ -16,5 +18,18 @@ public class Circle implements Shape {
     @Override
     public double getField() {
         return Math.PI * (radius * radius);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Circle circle = (Circle) o;
+        return Double.compare(circle.radius, radius) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(radius);
     }
 }
