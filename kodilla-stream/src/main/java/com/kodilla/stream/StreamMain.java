c63f59adc59f5fe8f780943b9864ba9a3be257ac
mrasinski <ariel_6@wp.pl>
2019-02-12 21:58:08 +0100

    7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,16 +1,21 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
 
+        /*
         PoemBeautifier poemBeautifier = new PoemBeautifier();
 
         poemBeautifier.beautify("Java", (textToDecorate) -> textToDecorate.toUpperCase());
         poemBeautifier.beautify("Intellij", (textToDecorate) -> "ABC" + textToDecorate + "ABC");
         poemBeautifier.beautify("asfnnlasf", (textToDecorate) -> textToDecorate.substring(2));
         poemBeautifier.beautify("ASFOIjifjaASd", (textToDecorate) -> textToDecorate.toLowerCase());
+        */
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
     }
 }
