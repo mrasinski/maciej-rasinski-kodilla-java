f3f22d01940504a6219fbdf2853e6732dff0b088
mrasinski <ariel_6@wp.pl>
2019-02-12 21:10:48 +0100

    7.1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -8,6 +8,9 @@ public class StreamMain {
         PoemBeautifier poemBeautifier = new PoemBeautifier();
 
         poemBeautifier.beautify("Java", (textToDecorate) -> textToDecorate.toUpperCase());
+        poemBeautifier.beautify("Intellij", (textToDecorate) -> "ABC" + textToDecorate + "ABC");
+        poemBeautifier.beautify("asfnnlasf", (textToDecorate) -> textToDecorate.substring(2));
+        poemBeautifier.beautify("ASFOIjifjaASd", (textToDecorate) -> textToDecorate.toLowerCase());
 
     }
 }
