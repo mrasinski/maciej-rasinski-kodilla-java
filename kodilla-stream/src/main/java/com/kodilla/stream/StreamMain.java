7e0b5ec051e3d0384f10afca491921a4a4a8a1ca
mrasinski <ariel_6@wp.pl>
2019-02-12 20:44:18 +0100

    Stream

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,13 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+
+public class StreamMain {
+    public static void main(String[] args) {
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        poemBeautifier.beautify("Java", (textToDecorate) -> textToDecorate.toUpperCase());
+
+    }
+}
