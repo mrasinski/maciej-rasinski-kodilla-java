89c8e962ad1396e3651338882ed2af12ade10914
mrasinski <ariel_6@wp.pl>
2019-02-13 08:26:40 +0100

    7.1 changes

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,21 +1,20 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.person.People;
 
 public class StreamMain {
     public static void main(String[] args) {
 
-        /*
         PoemBeautifier poemBeautifier = new PoemBeautifier();
 
-        poemBeautifier.beautify("Java", (textToDecorate) -> textToDecorate.toUpperCase());
-        poemBeautifier.beautify("Intellij", (textToDecorate) -> "ABC" + textToDecorate + "ABC");
-        poemBeautifier.beautify("asfnnlasf", (textToDecorate) -> textToDecorate.substring(2));
-        poemBeautifier.beautify("ASFOIjifjaASd", (textToDecorate) -> textToDecorate.toLowerCase());
-        */
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        poemBeautifier.beautify("Java", textToDecorate -> textToDecorate.toUpperCase());
+        poemBeautifier.beautify("Intellij", textToDecorate -> "ABC" + textToDecorate + "ABC");
+        poemBeautifier.beautify("asfnnlasf", textToDecorate -> textToDecorate.substring(2));
+        poemBeautifier.beautify("ASFOIjifjaASd", textToDecorate -> textToDecorate.toLowerCase());
 
+        People.getList().stream()
+                .map(s -> s.toUpperCase())
+                .forEach(System.out::println);
     }
 }
