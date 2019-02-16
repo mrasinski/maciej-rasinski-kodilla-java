6d0c7db94b9d681605e70467239a997d8dabb902
mrasinski <ariel_6@wp.pl>
2019-02-16 10:18:44 +0100

    7.5 update

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,15 +1,22 @@
 package com.kodilla.stream.array;
 
+import java.util.Arrays;
+import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
     static double getAverage(int[] numbers) {
-        int temp = 0;
-        IntStream.range(0, numbers.length)
-                .forEach(value -> System.out.println(numbers[value]));
+        Arrays.stream(numbers)
+                .forEach(System.out::println);
 
-        return IntStream.range(0, numbers.length)
-                .map(n -> temp + numbers[n])
-                .average().getAsDouble();
+        OptionalDouble result = IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .average();
+
+        if(result.isPresent()) {
+            return result.getAsDouble();
+        } else {
+            return 0.0;
+        }
     }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -9,13 +9,12 @@ public class ArrayOperationsTestSuite {
     public void testGetAverage() {
         //Given
         int[] numbers = new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
-        int[] numbers2 = new int[] {2,3,4,7,8,123,4125,616321,213,10,2,3,4,7,8,123,4125,616321,125,23,756,846,34,42,867,
-                2356,123578,987,7612};
+        int[] numbers2 = new int[] {80,45,4,64,40,1,5,14,14,63,54,13,35,100,30,98,100,4,43,21,52,84,71,64,63,51,93,56,89,23};
         //When
         double result = ArrayOperations.getAverage(numbers);
         double result2 = ArrayOperations.getAverage(numbers2);
         //Then
         Assert.assertEquals(15.5, result, 0.1);
-        Assert.assertEquals(41858.6, result2, 0.1);
+        Assert.assertEquals(49.1, result2, 0.1);
     }
 }
